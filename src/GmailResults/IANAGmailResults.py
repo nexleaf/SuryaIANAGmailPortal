9e04bc45ca64ffb96fa454f06a8185c73911b031
surya <surya@Peir2.cens.UCLA.EDU>
2010-11-24 22:45:09 -0800

    switched email responses to use django template system. We can now have various debug levels and configurations based on settings. One thing I do not like is that I am setting the template dir from and environment variable. That needs to chagne somehow

--- src/GmailResults/IANAGmailResults.py
+++ src/GmailResults/IANAGmailResults.py
@@ -3,11 +3,15 @@ Created on Nov 15, 2010
 
 @author: surya
 '''
+import os
 import sys
 import time
 import json
 import logging
 import smtplib
+
+from django.conf import settings as djsettings
+from django.template.loader import render_to_string
 
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
@@ -23,6 +27,11 @@ from GmailResultsFramework.GmailResultsFramework import GmailResultsFramework
 
 connect('SuryaDB')
 
+# Setup some variables for the djano template loader to work
+# Need to make this not be a call to set an environment variable... does pwd work from here?
+djsettings.configure(DEBUG=True, TEMPLATE_DEBUG=True, \
+                     TEMPLATE_DIRS=(os.getenv("RESULT_EMAIL_TEMPLATES"),))
+
 class IANAGmailResults(GmailResultsFramework):
     ''' This class implements the functionality to poll the SuryaDB
         for results on processing uploaded images and emails the 
@@ -31,8 +40,7 @@ class IANAGmailResults(GmailResultsFramework):
     
     def checkResults(self):
         ''' Refer GmailResultsFramework.checkResults for documentation
-        '''        
-        
+        '''
         tags = self.grestags + " IANA"
         
         gmail_user = setting.get("username")
@@ -69,43 +77,11 @@ class IANAGmailResults(GmailResultsFramework):
                 msghdr += "Subject: BC Results for " + str(item.computationResult.chartImage.name) + '\n'
                 
                 msg = MIMEMultipart('localhost')
-                                   
-                # attach preProcessingResult.
-                text = 'Results on PREPROCESSING the Image for configuration: \n'
-                preProcessingConfiguration = item.preProcessingConfiguration
-                text += 'PreProcessing Parameters Used: \n'
-                text += 'dp           : '+str(preProcessingConfiguration.dp) + '\n'
-                text += 'minimumRadius: '+str(preProcessingConfiguration.minimumRadius) + '\n'
-                text += 'maximumRadius: '+str(preProcessingConfiguration.maximumRadius) + '\n'
-                text += 'highThreshold: '+str(preProcessingConfiguration.highThreshold) + '\n'
-                text += 'accumulatorThreshold: '+str(preProcessingConfiguration.accumulatorThreshold) + '\n'
-                text += 'minimumDistance: '+str(preProcessingConfiguration.minimumDistance) + '\n'
-                text += 'samplingFactor : '+str(preProcessingConfiguration.samplingFactor) + '\n\n'
                 
-                text += 'Results: \n'
-                text += 'SampledRGB  : ' + str(item.preProcessingResult.sampled) + '\n'
-                text += 'DebugPicName: '+item.preProcessingResult.debugImage.name + '\n\n'
-                
-                # attach computation Result.
-                text += 'Results on COMPUTATION of the Results: \n'
-                computationConfiguration = item.computationConfiguration
-                text += 'Computation parameters Used: \n'
-                text += 'filterRadius: ' + str(computationConfiguration.filterRadius) + '\n'
-                text += 'exposedTime : ' + str(computationConfiguration.exposedTime) + '\n'
-                text += 'bcStrip     : ' + str(item.bcStrips.bcStrips) + '\n'
-                text += 'airFlowRate : ' + str(computationConfiguration.airFlowRate) + '\n\n'
-
-                result = item.computationResult.result
-                text += 'Results: \n'
-                text += 'BCAreaRed    : ' + str(result.BCAreaRed) + '\n'
-                text += 'BCAreaGreen  : ' + str(result.BCAreaGreen) + '\n'
-                text += 'BCAreaBlue   : ' + str(result.BCAreaBlue) + '\n'
-                text += 'BCVolRed     : ' + str(result.BCVolRed) + '\n'
-                text += 'BCVolGreen   : ' + str(result.BCVolGreen) + '\n'
-                text += 'BCVolBlue    : ' + str(result.BCVolBlue) + '\n\n'
-                text += 'chartFileName: ' + str(item.computationResult.chartImage.name) + '\n'
-                
+                text = render_to_string("result_email_debug.html", {'item': item})
+                                
                 textmsg = MIMEText(text)
+                
                 msg.attach(textmsg)
 
                 smtpserver.sendmail(sendFrom, misc_dict["fromemail"], msghdr + msg.as_string())
@@ -138,78 +114,9 @@ class IANAGmailResults(GmailResultsFramework):
                 msghdr += "Subject: BC Results for " + str(item.computationResult.chartImage.name) + '\n'
                 
                 msg = MIMEMultipart('localhost')
-                    
-                # if item failed in PPROCCALIB PHASE
-                if "PPROCCALIB" in item.status:
-                    text = "Failed to Fetch PreProcessingData for item : "+item.item.file.name+'\n\n'
-                # if item failed in PPROC PHASE
-                elif "PPROC" in item.status:
-                    text = "Failed to PreProcess the item: "+item.item.file.name+"\n\n"
-                    preProcessingConfiguration = item.preProcessingConfiguration
-                    text += 'PreProcessing Parameters Used: \n'
-                    text += 'dp           : '+str(preProcessingConfiguration.dp) + '\n'
-                    text += 'minimumRadius: '+str(preProcessingConfiguration.minimumRadius) + '\n'
-                    text += 'maximumRadius: '+str(preProcessingConfiguration.maximumRadius) + '\n'
-                    text += 'highThreshold: '+str(preProcessingConfiguration.highThreshold) + '\n'
-                    text += 'accumulatorThreshold: '+str(preProcessingConfiguration.accumulatorThreshold) + '\n'
-                    text += 'minimumDistance: '+str(preProcessingConfiguration.minimumDistance) + '\n'
-                    text += 'samplingFactor : '+str(preProcessingConfiguration.samplingFactor) + '\n\n'
-                    text += 'DebugPicName   : '+str(item.preProcessingResult.debugImage.name) + '\n\n'
-                    
-                # if item failed in COMPUCALIB PHASE
-                elif "COMPUCALIB" in item.status:
-                    text = "Failed to Fetch Computation Calibration info: "+item.item.file.name+"\n\n"
-                    
-                    text += 'Results on PREPROCESSING the Image for configuration: \n'
-                    preProcessingConfiguration = item.preProcessingConfiguration
-                    text += 'PreProcessing Parameters Used: \n'
-                    text += 'dp           : '+str(preProcessingConfiguration.dp) + '\n'
-                    text += 'minimumRadius: '+str(preProcessingConfiguration.minimumRadius) + '\n'
-                    text += 'maximumRadius: '+str(preProcessingConfiguration.maximumRadius) + '\n'
-                    text += 'highThreshold: '+str(preProcessingConfiguration.highThreshold) + '\n'
-                    text += 'accumulatorThreshold: '+str(preProcessingConfiguration.accumulatorThreshold) + '\n'
-                    text += 'minimumDistance: '+str(preProcessingConfiguration.minimumDistance) + '\n'
-                    text += 'samplingFactor : '+str(preProcessingConfiguration.samplingFactor) + '\n\n'
-                    
-                    text += 'Results: \n'
-                    text += 'SampledRGB  : ' + str(item.preProcessingResult.sampled) + '\n'
-                    text += 'DebugPicName: '+str(item.preProcessingResult.debugImage.name) + '\n\n'
-                    
-                # if item failed in COMPU PHASE
-                elif "COMPU" in item.status:
-                        text = "Failed to Computate Results: "+item.item.file.name+"\n\n"
-                        
-                        text += 'Results on PREPROCESSING the Image for configuration: \n'
-                        preProcessingConfiguration = item.preProcessingConfiguration
-                        text += 'PreProcessing Parameters Used: \n'
-                        text += 'dp           : '+str(preProcessingConfiguration.dp) + '\n'
-                        text += 'minimumRadius: '+str(preProcessingConfiguration.minimumRadius) + '\n'
-                        text += 'maximumRadius: '+str(preProcessingConfiguration.maximumRadius) + '\n'
-                        text += 'highThreshold: '+str(preProcessingConfiguration.highThreshold) + '\n'
-                        text += 'accumulatorThreshold: '+str(preProcessingConfiguration.accumulatorThreshold) + '\n'
-                        text += 'minimumDistance: '+str(preProcessingConfiguration.minimumDistance) + '\n'
-                        text += 'samplingFactor : '+str(preProcessingConfiguration.samplingFactor) + '\n\n'
-                        
-                        text += 'Results: \n'
-                        text += 'SampledRGB  : ' + str(item.preProcessingResult.sampled) + '\n'
-                        text += 'DebugPicName: '+str(item.preProcessingResult.debugImage.name) + '\n\n'
-                        
-                        # attach computation Result.
-                        text += 'Results on COMPUTATION of the Results: \n'
-                        computationConfiguration = item.computationConfiguration
-                        text += 'Computation parameters Used: \n'
-                        text += 'filterRadius: ' + str(computationConfiguration.filterRadius) + '\n'
-                        text += 'exposedTime : ' + str(computationConfiguration.exposedTime) + '\n'
-                        text += 'bcStrip     : ' + str(item.bcStrips.bcStrips) + '\n'
-                        text += 'airFlowRate : ' + str(computationConfiguration.airFlowRate) + '\n\n'
 
-                # if item failed in SAVIN PHASE
-                elif "SAVIN" in item.status:
-                        text = "Failed to Save Results for item : "+item.item.file.name+'\n\n'
+                text = render_to_string("failed_email_debug.html", {'item': item})
                 
-                else:
-                        text = "Failed processing item: "+item.item.file.name+'\n'
-                        
                 msgtext = MIMEText(text)
                 msg.attach(msghdr + msgtext)
                 smtpserver.sendmail(send_from, misc_dict["fromemail"], msg.as_string())
